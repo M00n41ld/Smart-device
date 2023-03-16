a6aaf613e64b8e2837dbbb8d6b791644f6e44caf
Мария <maryreznik82@gmail.com>
2023-03-16 19:42:42 +0400

    добавляет блок о компании

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,70 @@ import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 // import './modules/accordion';
 import { initAccordions } from './modules/accordion/init-accordion';
+
+
+const buttonMore = document.querySelector('.about__button')
+const lastParagraph = document.querySelector('.about__text--target')
+const mobileParagraph = document.querySelector('.about__text--target-mobile')
+console.log(lastParagraph)
+const mediaQuery = window.matchMedia("(max-width: 767px)")
+console.log(mediaQuery.matches)
+
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (e.matches) {
+    console.log('mobile')
+    buttonMore.addEventListener('click', ShowMoreMobile)
+  }
+  else {
+    console.log('desktop')
+    buttonMore.addEventListener('click', ShowMore)
+    // buttonMore.addEventListener('click', () => console.log('событие'))
+  }
+}
+
+// Register event listener
+window.addEventListener('change', handleTabletChange)
+
+// Initial check
+handleTabletChange(mediaQuery)
+
+function ShowMoreMobile () {
+  mobileParagraph.classList.remove('about__text--mobile')
+  lastParagraph.classList.remove('about__text--hidden')
+  // lastParagraph.style.height = 'fit-content';
+  buttonMore.removeEventListener('click', ShowMoreMobile)
+  buttonMore.addEventListener('click', ShowLessMobile)
+  buttonMore.textContent = "Свернуть"
+
+}
+function ShowLessMobile () {
+  mobileParagraph.classList.add('about__text--mobile')
+  lastParagraph.classList.add('about__text--hidden')
+  buttonMore.removeEventListener('click', ShowLessMobile)
+  buttonMore.addEventListener('click', ShowMoreMobile)
+  buttonMore.textContent = "Подробнее"
+
+}
+
+function ShowMore () {
+  lastParagraph.classList.remove('about__text--hidden')
+  // lastParagraph.style.height = 'fit-content';
+  buttonMore.removeEventListener('click', ShowMore)
+  buttonMore.addEventListener('click', ShowLess)
+  buttonMore.textContent = "Свернуть"
+}
+
+
+function ShowLess () {
+  lastParagraph.classList.add('about__text--hidden')
+  buttonMore.removeEventListener('click', ShowLess)
+  buttonMore.addEventListener('click', ShowMore)
+  buttonMore.textContent = "Подробнее"
+}
+
+
+
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
