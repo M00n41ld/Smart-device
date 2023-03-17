3a9bc12a6939c4ce8906428bfce0ad4218c288ff
Мария <maryreznik82@gmail.com>
2023-03-17 23:15:49 +0400

    добавляет блок о компании

--- source/js/main.js
+++ source/js/main.js
@@ -2,70 +2,55 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 // import './modules/accordion';
-import { initAccordions } from './modules/accordion/init-accordion';
+import {initAccordions} from './modules/accordion/init-accordion';
 
 
-const buttonMore = document.querySelector('.about__button')
-const lastParagraph = document.querySelector('.about__text--target')
-const mobileParagraph = document.querySelector('.about__text--target-mobile')
-console.log(lastParagraph)
-const mediaQuery = window.matchMedia("(max-width: 767px)")
-console.log(mediaQuery.matches)
+const buttonMore = document.querySelector('.about__button');
+const lastParagraph = document.querySelector('.about__text--target');
+const mobileParagraph = document.querySelector('.about__text--target-mobile');
+const mediaQuery = window.matchMedia('(max-width: 767px)');
 
 function handleTabletChange(e) {
-  // Check if the media query is true
   if (e.matches) {
-    console.log('mobile')
-    buttonMore.addEventListener('click', ShowMoreMobile)
-  }
-  else {
-    console.log('desktop')
-    buttonMore.addEventListener('click', ShowMore)
-    // buttonMore.addEventListener('click', () => console.log('событие'))
+    buttonMore.addEventListener('click', ShowMoreMobile);
+  } else {
+    buttonMore.addEventListener('click', ShowMore);
   }
 }
 
-// Register event listener
-window.addEventListener('change', handleTabletChange)
+window.addEventListener('change', handleTabletChange);
 
-// Initial check
-handleTabletChange(mediaQuery)
+handleTabletChange(mediaQuery);
 
-function ShowMoreMobile () {
-  mobileParagraph.classList.remove('about__text--mobile')
-  lastParagraph.classList.remove('about__text--hidden')
-  // lastParagraph.style.height = 'fit-content';
-  buttonMore.removeEventListener('click', ShowMoreMobile)
-  buttonMore.addEventListener('click', ShowLessMobile)
-  buttonMore.textContent = "Свернуть"
-
-}
-function ShowLessMobile () {
-  mobileParagraph.classList.add('about__text--mobile')
-  lastParagraph.classList.add('about__text--hidden')
-  buttonMore.removeEventListener('click', ShowLessMobile)
-  buttonMore.addEventListener('click', ShowMoreMobile)
-  buttonMore.textContent = "Подробнее"
-
+function ShowMoreMobile() {
+  mobileParagraph.classList.remove('about__text--mobile');
+  lastParagraph.classList.remove('about__text--hidden');
+  buttonMore.removeEventListener('click', ShowMoreMobile);
+  buttonMore.addEventListener('click', ShowLessMobile);
+  buttonMore.textContent = 'Свернуть';
 }
 
-function ShowMore () {
-  lastParagraph.classList.remove('about__text--hidden')
-  // lastParagraph.style.height = 'fit-content';
-  buttonMore.removeEventListener('click', ShowMore)
-  buttonMore.addEventListener('click', ShowLess)
-  buttonMore.textContent = "Свернуть"
+function ShowLessMobile() {
+  mobileParagraph.classList.add('about__text--mobile');
+  lastParagraph.classList.add('about__text--hidden');
+  buttonMore.removeEventListener('click', ShowLessMobile);
+  buttonMore.addEventListener('click', ShowMoreMobile);
+  buttonMore.textContent = 'Подробнее';
 }
 
-
-function ShowLess () {
-  lastParagraph.classList.add('about__text--hidden')
-  buttonMore.removeEventListener('click', ShowLess)
-  buttonMore.addEventListener('click', ShowMore)
-  buttonMore.textContent = "Подробнее"
+function ShowMore() {
+  lastParagraph.classList.remove('about__text--hidden');
+  buttonMore.removeEventListener('click', ShowMore);
+  buttonMore.addEventListener('click', ShowLess);
+  buttonMore.textContent = 'Свернуть';
 }
 
-
+function ShowLess() {
+  lastParagraph.classList.add('about__text--hidden');
+  buttonMore.removeEventListener('click', ShowLess);
+  buttonMore.addEventListener('click', ShowMore);
+  buttonMore.textContent = 'Подробнее';
+}
 
 // ---------------------------------
 
